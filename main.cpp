5f15bacf48317952521f9c9d5e2f6db025aa2f74
T20169380 <zachary.foster2@tulsacc.edu>
2019-02-07 10:49:49 -0600

    Finished the program

--- main.cpp
+++ main.cpp
@@ -13,7 +13,11 @@ int main() {
     getline(cin, userLine);
     cout << "You entered: " << userLine << endl;
 
-
+    userLine.replace(userLine.find("TTYL"), 4, "Talk to you later");
+    userLine.replace(userLine.find("IDK"), 3, "I don't know");
+    userLine.replace(userLine.find("BFF"), 3, "best friend forever");
+    userLine.replace(userLine.find("JK"), 2, "just kidding");
+    userLine.replace(userLine.find("TMI"), 3, "too much information");
 
     cout << "Expanded: " << userLine << endl;
 
